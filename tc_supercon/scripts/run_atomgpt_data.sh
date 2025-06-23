aae3e38408793cfbdcecdea6cbffd17e3550230e
Campbell <crc00042@mix.wvu.edu>
2025-06-23 10:15:38 -0600

    initialize small dataset for atomgpt devving

---
+++ tc_supercon/scripts/run_atomgpt_data.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+set -e
+mkdir -p atomgpt_data
+uv pip install jarvis-tools pymatgen numpy pandas tqdm
+python scripts/data_preprocess.py atomgpt  --dataset dft_3d --output ./atomgpt_data  \
+                                           --target Tc_supercon --max-size 25 --seed 123
+rm -rf JVASP*
